9048c48e9bbab2cab4c88fcc0378a3d4a334a633
Yuzhou Ge <yuzhouge@usc.edu>
2020-01-09 14:16:35 -0800

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Yuzhou Ge on 1/9/20.
+//  Copyright © 2020 haodan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
