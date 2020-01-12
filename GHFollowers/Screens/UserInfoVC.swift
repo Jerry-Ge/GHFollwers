d238e0d569fe153bcb296eb118cf3b271a9cf9a5
Yuzhou Ge <yuzhouge@usc.edu>
2020-01-12 10:36:53 -0800

    created blank user info modal sheet

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Yuzhou Ge on 1/12/20.
+//  Copyright © 2020 haodan. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+    
+}
